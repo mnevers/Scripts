6d7df1048694b40fcd7f45b5a24f05a7e97a4efe
mnevers <42180713+mnevers@users.noreply.github.com>
2021-08-09 11:39:16 -0400

    Add files via upload

---
+++ MobileNumberChange.ps1
@@ -0,0 +1,34 @@
+﻿# MobileNumberChange.ps1
+#
+# This PowerShell script reads in the mobile number attribute from all AD users in a give OU
+# Removes unncessary characters I.E " " "()" etc
+# Adds a leading + and a 1 for USA country code if necessary
+# Script will work on phone numbers that may have already been formatted properly as well
+# Ignores blank mobile fields
+#
+# Author: Matthew Nevers
+
+function SetPhone {
+    Param(
+        [Parameter(Position=0,Mandatory=$true)][String[]]$UserName,
+        [Parameter(Position=1,Mandatory=$true)][String]$PhoneNum
+    )
+    echo $UserName
+    echo $PhoneNum
+
+    Get-ADUser -Filter "Name -eq '$UserName'" -Properties mobile | ForEach-Object { Set-ADUser $_ -mobile $PhoneNum }
+}
+
+$User = Get-ADUser -Filter * -SearchBase "OU=Employees,OU=Users,OU=Company,DC=Company,DC=lan" -Properties mobile
+$count = 0    
+
+$User | ForEach-Object  {
+    $intphone = $_.mobile
+    $intphone = $intphone -replace "[^0-9]"
+    if($intphone.StartsWith(1)) {$intphone = "+" + $intphone} else {$intphone = "+1" + $intphone}
+    
+    if ($intphone -eq "+1") {echo "Ignore Empty Field"} else { SetPhone $_.Name $intphone }
+    
+}
+
+
